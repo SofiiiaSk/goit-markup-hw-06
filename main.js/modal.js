a7faa398d8aa1d6c8067864676f5628c4973146d
SofiiiaSk <sopfiasheveleva@gmail.com>
2026-07-29 00:15:22 +0300

    hm6

--- main.js/modal.js
+++ main.js/modal.js
@@ -1,6 +1,9 @@
 const modal = document.querySelector("#modal");
 const openButton = document.querySelector(".one-button");
 const closeButton = document.querySelector(".modal-close");
+const mobileMenu = document.querySelector(".mobile-menu");
+const burgerButton = document.querySelector(".burger-button");
+const mobileCloseButton = document.querySelector(".mobile-menu-close");
 
 function openModal() {
   modal.classList.add("is-open");
@@ -26,3 +29,12 @@ document.addEventListener("keydown", (event) => {
     closeModal();
   }
 });
+
+function setMobileMenu(open) {
+  mobileMenu.classList.toggle("is-open", open);
+  burgerButton.setAttribute("aria-expanded", String(open));
+  document.body.style.overflow = open ? "hidden" : "";
+}
+
+burgerButton.addEventListener("click", () => setMobileMenu(true));
+mobileCloseButton.addEventListener("click", () => setMobileMenu(false));
